303ec3ebe22f371366d9c7b2637ed08aa33071c1
gabi fabri <00001106292613sp@al.educacao.sp.gov.br>
2024-09-09 18:11:47 -0300

    Create 04_mix_up.py

---
+++ 04_mix_up.py
@@ -0,0 +1,50 @@
+
+"""
+04. mix_up
+
+Dadas as strings a e b, retorne uma string com a e b separados
+por um espaço '<a> <b>', além disso, troque os 2 primeiros caracteres
+das duas strings.
+
+Exemplo:
+    'mix', 'pod' -> 'pox mid'
+    'dog, 'dinner' -> 'dig donner'
+
+Assuma que a e b tem tamanho 2 ou maior.
+"""
+def mix_up(a, b):
+    a_prefix, a_suffix = a[:2], a[2:]
+    b_prefix, b_suffix = b[:2], b[2:]
+    
+    new_a = b_prefix + a_suffix
+    new_b = a_prefix + b_suffix
+
+    # Retornar a string formatada com as novas strings separadas por um espaço
+    return f'{new_a} {new_b}'
+
+
+# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+
+def test(f, in_, expected):
+    """
+    Executa a função f com o parâmetro in_ e compara o resultado com expected.
+    :return: Exibe uma mensagem indicando se a função f está correta ou não.
+    """
+    out = f(*in_)
+
+    if out == expected:
+        sign = '✅'
+        info = ''
+    else:
+        sign = '❌'
+        info = f'e o correto é {expected!r}'
+
+    print(f'{sign} {f.__name__}{in_!r} retornou {out!r} {info}')
+
+
+if __name__ == '__main__':
+    # Testes que verificam o resultado do seu código em alguns cenários.
+    test(mix_up, ('mix', 'pod'), 'pox mid')
+    test(mix_up, ('dog', 'dinner'), 'dig donner')
+    test(mix_up, ('gnash', 'sport'), 'spash gnort')
+    test(mix_up, ('pezzy', 'firm'), 'fizzy perm')
